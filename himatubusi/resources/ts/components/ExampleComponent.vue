a29fe7e135d64d9ca1ca118ae5fb3d7c3dc2da19
yamamoto hiroki <h_yamamoto@sports-f.co.jp>
2021-02-25 19:01:26 +0900

    色々やってる

--- himatubusi/resources/ts/components/ExampleComponent.vue
+++ himatubusi/resources/ts/components/ExampleComponent.vue
@@ -1,35 +1,46 @@
 <template>
     <YouTube
+        :vars="playerVars"
         :src="src"
-        @ready="onReady"
         ref="youtube" />
 
 </template>
 
 <script lang="ts">
-    import { defineComponent,ref,onMounted,SetupContext } from "vue";
+    import { defineComponent,ref,onMounted,SetupContext, reactive } from "vue";
     import YouTube from 'vue3-youtube'
+    import axios from 'axios'
     export default defineComponent({
         components: {
             YouTube
         },
         setup(context:SetupContext) {
+            const created = async() => {
+                const lists = await axios.get('/youtube')
+                ids.value = lists.data.map((v: { id: { videoId: string; }; }) => {
+                    return v.id.videoId
+                }).join()
+            }
+            created()
             const videoId = 'lG0Ys-2d4MA';
-            const url = "https://www.youtube.com/watch?v=" + videoId
-            const src = ref(url)
+            // const url = "https://www.youtube.com/watch?v=" + videoId +'?autoplay=1'
+            // const src = ref(url)
+            const ids = ref<string[]>([])
             const youtube = ref<InstanceType<typeof YouTube>>()// 1: 型を指定
-
+            const playerVars = reactive({
+                    autoplay: 1,
+                    controls: 0,
+                    playlist: ids.value
+            });
             onMounted(() => {
                 if (!youtube.value) return // 型から undefined をなくす
             })
-            const onReady = () => {
-                youtube.value?.playVideo()
-            }
 
             return {
+              playerVars,
               src,
               youtube,
-              onReady
+              urls
             };
         },
 
