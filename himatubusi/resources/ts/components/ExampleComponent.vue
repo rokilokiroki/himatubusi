d6456f82293686b305dd8a5c3df1b085f526bcd2
yamamoto hiroki <h_yamamoto@sports-f.co.jp>
2021-03-04 18:07:29 +0900

    [change] autoloadができる playlistが動く

--- himatubusi/resources/ts/components/ExampleComponent.vue
+++ himatubusi/resources/ts/components/ExampleComponent.vue
@@ -1,46 +1,58 @@
 <template>
-    <YouTube
+    <div>
+        <Loading :show_loading="show_loading"></Loading>
+        <YouTube
+        v-if="show_loading === false"
         :vars="playerVars"
         :src="src"
+        @ready="onReady"
         ref="youtube" />
-
+    </div>
 </template>
 
 <script lang="ts">
     import { defineComponent,ref,onMounted,SetupContext, reactive } from "vue";
     import YouTube from 'vue3-youtube'
+    import Loading from './Loading.vue'
     import axios from 'axios'
     export default defineComponent({
         components: {
-            YouTube
+            YouTube,
+            Loading
         },
         setup(context:SetupContext) {
+            const src = ref("")
+            const show_loading = ref(true)
+            const playlist = ref("")
+            const youtube = ref<InstanceType<typeof YouTube>>()// 1: 型を指定
+            const playerVars = reactive({
+                    autoplay: 1,
+                    playlist: playlist.value
+            });
+            const onReady = () => {
+                youtube.value?.playVideo()
+            }
             const created = async() => {
                 const lists = await axios.get('/youtube')
                 const videoIds = lists.data.map((v: { id: { videoId: string; }; }) => {
                     return v.id.videoId
                 })
                 src.value = videoIds.shift()
-                playlist.value = videoIds.join()
+                playerVars.playlist = videoIds.join()
+                show_loading.value = false
             }
             created()
-            const videoId = 'lG0Ys-2d4MA';
-            // const url = "https://www.youtube.com/watch?v=" + videoId +'?autoplay=1'
-            const src = ref("")
-            const playlist = ref<string[]>([])
-            const youtube = ref<InstanceType<typeof YouTube>>()// 1: 型を指定
-            const playerVars = reactive({
-                    autoplay: 1,
-                    playlist: playlist.value
-            });
             onMounted(() => {
                 if (!youtube.value) return // 型から undefined をなくす
             })
 
+
             return {
               playerVars,
+              onReady,
               src,
               youtube,
+              show_loading
             };
         },
 
---
+++ himatubusi/resources/ts/components/Loading.vue
@@ -0,0 +1,35 @@
+<style scoped>
+  .loading {
+      width: 100%;
+      text-align: center;
+  }
+
+</style>
+
+<template>
+    <div v-if="show_loading" class="fixLoaderWrap">
+        <div class="fixLoaderBox">
+            <div uk-spinner="ratio: 4" class="loading"></div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import 'uikit/dist/css/uikit.css'
+  import 'uikit/dist/css/uikit.min.css'
+  type Props = {
+      show_loading: Boolean
+  }
+  export default defineComponent({
+      props: {
+          show_loading:{
+              type: Boolean,
+              default: true
+          }
+      },
+      setup(props: Props){
+          props.show_loading
+      }
+  })
+</script>
